5343b3281b49d655bec0b6eb05213c5e3dece2f8
Tomas Muruchi <155777084+Tomyrex2780@users.noreply.github.com>
2025-05-19 01:34:26 -0300

    assa

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for, session , flash
 from main import ConexionSFTP
 
 app = Flask(__name__)
 app.secret_key = "clave_super_secreta"
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    archivos = ["archivo1.txt", "archivo2.txt", "archivo3.txt"]
-    if request.method == 'POST':        # Guardamos la lista de archivos en la sesión
-        session['archivos'] = archivos
-        return redirect(url_for('listar_archivos'))
-    return render_template('index.html')
+    if request.method == 'POST':
+        conexion_info = {
+            'hostname': request.form['hostname'],
+            'username': request.form['username'],
+            'password': request.form['password']
+        }
+        session['conexion_info'] = conexion_info
+        # Aca se crea una instancia de la clase conexionSFTP
+        session['message'] = "Conexión establecida!"
+        return redirect(url_for('index'))
+    conexion_data = session.pop('conexion_info', None)
+    message = session.pop('message', None)
+    return render_template('index.html', conexion=conexion_data, message=message)
+
 
 @app.route('/enviar_archivo', methods=['GET', 'POST'])
 def enviar_archivo():
     if request.method == 'POST':
-        pass  # lógica de envío aquí
-    return render_template('enviar_archivo.html')  # ¡Faltaba return!
+        'ruta_archivo' == request.files["archivo"].filename
+        return redirect(url_for('index', message='Archivo enviado!'))
+    return render_template('enviar_archivo.html')
 
 @app.route('/listar_archivos', methods=['GET', 'POST'])
 def listar_archivos():
@@ -28,16 +39,18 @@ def listar_archivos():
 @app.route('/descargar_archivo', methods=['GET', 'POST'])
 def descargar_archivo():
     if request.method == 'POST':
-        pass
-
+        pass # lógica de descarga aquí
     return render_template('descarga.html')
 
 @app.route('/borrar_archivo', methods=['GET', 'POST'])
 def borrar_archivo():
     if request.method == 'POST':
-        pass
-  
+        pass # lógica de borrado aquí
     return render_template('borrar.html')
+
+@app.route('/desconectar', methods=['GET', 'POST'])
+def desconectar():
+    return render_template('index.html', conexion=None, message=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
